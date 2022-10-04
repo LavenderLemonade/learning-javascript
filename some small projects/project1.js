ccfb8f46301fe4f6f9f2db053413b4bc4bf97dd9
Sam Elliott <soe1@uakron.edu>
2022-10-04 04:32:44 -0400

    burning more midnight oil

---
+++ some small projects/project1.js
@@ -0,0 +1,12 @@
+//What if I wanted to make a tip calculator here in JS? 
+
+//So, let's outline what it needs to do:
+    // 1. Take in the amount for a meal 
+    // 2. Calculate sales tax on that meal and then apply that. 
+    // 3. Ask the user if they want to do a 10%, 15%, or 20% tip on top of that 
+    // 4. Spit out a nice message that thanks the users and says how much they will be paying in total
+
+//So, what do I need to do in order to address each problem? 
+
+//first things first, let's read some user input. 
+let orderCost = window.prompt("How much did your meal cost?"); //this will prompt the user for a total 
