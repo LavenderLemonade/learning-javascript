af2bed493285cdf69d00b7a9668b1c08c03438e2
Sam Elliott <soe1@uakron.edu>
2022-10-04 04:33:53 -0400

    lmao I keep forgetting to save before I push let's rectify that one

--- some small projects/project1.js
+++ some small projects/project1.js
@@ -8,5 +8,22 @@
 
 //So, what do I need to do in order to address each problem? 
 
+//So, to keep in mind with OOP principles, I feel like I should make an object for the food being ordered. 
+
+let foodItem = 
+{
+    foodName : null, // this is going to be the title of the dish
+    foodCost : null, // this is going to be how much the dish costs 
+
+}
+
+//following that, and wanting to continue with OOP principles, I feel like making a function that takes in a 
+    //foodItem object will be a good next step, and then all of the calculation can happen there 
+
+function salesTax(foodItem)
+{
+    return ((foodItem.foodCost) * 0.0575) + foodItem.foodCost;// what this does is finds the tax (5.75% in Ohio) and then adds that to the total 
+}
+
 //first things first, let's read some user input. 
 let orderCost = window.prompt("How much did your meal cost?"); //this will prompt the user for a total 
